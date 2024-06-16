80b725b530228c8219aa907ffdc2f67659da496f
Zaid Sharieff <zaidsharieff09@gmail.com>
2024-06-17 00:02:30 +0530

    Add smooth scrolling functionality to navigation links

--- src/Nav.js
+++ src/Nav.js
@@ -1,32 +1,36 @@
 import './Nav.css';
 
+const scrollToSection = (sectionId) => {
+    return () => {
+        const yOffset = -150;
+        const element = document.getElementById(sectionId);
+        const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+
+        window.scrollTo({top: y, behavior: 'smooth'});
+    }
+}
+
 function Nav() {
     return (
             <div id="nav">
                 <div id="nav-left">
-                    <div id="hamburger">
-                        <img className='link' src='./hamburger icon.png' alt='ham'></img>
-                    </div>
                     <div id="name-nav">Zaid Sharieff</div>
                 </div>
                 <div id="nav-right">
-                    <a className="hash-link" href="#about-section">
-                        <div className="clickable" id="about">About</div>
-                    </a>
-                    <a className="hash-link" href="#portfolio-section">
+                    <div className="hash-link" onClick={scrollToSection('about-section')}>
+                        <div className="clickable" id="about" >About</div>
+                    </div>
+                    <div className="hash-link" onClick={scrollToSection('portfolio-section')}>
                         <div className="clickable" id="portfolio">Portfolio</div>
-                    </a>
-                    <a className="hash-link" href="#interests-section">
+                    </div>
+                    <div className="hash-link" onClick={scrollToSection('interests-section')}>
                         <div className="clickable" id="interest">Interest</div>
-                    </a>
-                    <a className="hash-link" href="#projects-section">
+                    </div>
+                    <div className="hash-link" onClick={scrollToSection('projects-section')}>
                         <div className="clickable" id="project">Project</div>
-                    </a>
-                    <a className="hash-link" href="#contact-section">
+                    </div>
+                    <div className="hash-link" onClick={scrollToSection('contact-section')}>
                         <div className="clickable" id="contact">Contact</div>
-                    </a>
-                    <div id="options">
-                        <img className="link" src='./options icon.png' alt='options'></img>
                     </div>
                 </div>
             </div>
